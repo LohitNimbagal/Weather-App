374c2996bd2d52c6cdca62435408dc53f54caf99
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2023-07-25 22:44:16 +0530

    new file:   index.html new file:   script.js new file:   style.css

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputBox = document.querySelector('inputBox');
+const searchBtn = document.getElementById('searchBtn');
+const weatherImg  = document.querySelector('.weatherImg');
+const temperature  = document.querySelector('.temperature');
+const description  = document.querySelector('.description');
+const humidity  = document.querySelector('humidity');
+const windspeed  = document.querySelector('windspeed');
+    
+async function checkWeather(city){
+    const apiKey = "921737e48b030dc8a213b843f99f2d40";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const weatherData = await fetch(`${url}`).then(response => response.json());
+
+    console.log(weatherData);
+}
+
+// searchBtn.addEventListener('click', () => {
+//     checkWeather(inputBox.value);
+// });
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
